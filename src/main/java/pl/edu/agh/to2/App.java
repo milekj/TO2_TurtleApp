28aaf2de1a6cc440c061c1c328c29784d459753d
mjanowsk <m.i.l.e.k.j.a.n@gmail.com>
2018-11-15 20:14:29 +0100

    removed classes from example commit

--- src/main/java/pl/edu/agh/to2/App.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.agh.to2;
-
-public class App {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/pl/edu/agh/to2/Main.java
+++ src/main/java/pl/edu/agh/to2/Main.java
@@ -2,6 +2,5 @@ package pl.edu.agh.to2;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
