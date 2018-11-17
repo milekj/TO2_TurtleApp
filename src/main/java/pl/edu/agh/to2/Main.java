ea11314bd42acda3cc696c68c8d465bba4e13bd8
bpawel10 <bpawel10@vp.pl>
2018-11-17 19:23:18 +0100

    Simple GUI example not working

--- src/main/java/pl/edu/agh/to2/Main.java
+++ src/main/java/pl/edu/agh/to2/Main.java
@@ -1,6 +1,36 @@
 package pl.edu.agh.to2;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private Stage primaryStage;
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Turtle App");
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/View.fxml"));
+            BorderPane rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
